aec11f2ed085a606dce4cc0dcf4bfc1bee3d0225
Donna <127694064+Donna008@users.noreply.github.com>
2025-02-05 11:24:18 -0700

    LC-two-sum [Runtime: 3 ms; Memory: 18.9 MB]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -3,9 +3,16 @@ class Solution:
         # one list nums, one target number
         # task two numbers = target
         #  find first num
-        for i in range(len(nums)):
-            # find second one
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-
+        # for i in range(len(nums)):
+        #     # find second one
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i,j]
+        # hashmap
+        #  make an empty map
+        map = {}
+        for i, num in enumerate(nums):
+            value = target - num
+            if value in map:
+                return [map[value],i]
+            map[num] = i
