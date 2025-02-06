6df7d9822327090debab16996314c2eca06b00b4
Donna <127694064+Donna008@users.noreply.github.com>
2025-02-05 22:48:48 -0700

    LC-longest-substring-without-repeating-characters [Runtime: 519 ms; Memory: 17.8 MB]

---
+++ hash-table/longest-substring-without-repeating-characters.py
@@ -0,0 +1,58 @@
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+        # print(s)
+        # i=0
+        # j=0
+        # record = {}
+        # blocking = None
+        
+        # if len(s) == 0:
+        #     return 0
+        # max = 1
+        # record[s[0]] = 1
+        # while(j<len(s)):
+        #     print(i,j)
+        #     if blocking == None:
+        #         j += 1
+        #         if j >= len(s):
+        #             break
+        #         if s[j] in record:
+        #             print('blocking', s[j])
+        #             blocking = s[j]
+        #             continue
+        #         else:
+        #             print('record add', s[j])
+        #             record[s[j]] = 1
+        #             if (j-i + 1) > max:
+        #                 max = j - i + 1
+        #     else:
+        #         if blocking == s[i]:
+        #             print('deblocking', blocking)
+        #             blocking = None
+        #         else:
+        #             print('record del', s[i])
+        #             del record[s[i]]
+        #         i += 1
+        #         if (j-i + 1) > max:
+        #                 max = j - i + 1
+        #     pass
+        # return max
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # 
+        max_length = 0
+        for i in range(len(s)):
+            chars = set()
+            for j in range(i,len(s)):
+                if s[j] in chars:
+                    break
+                chars.add(s[j])
+                max_length = max(max_length, j-i+1)
+        return max_length
+
+
+
+
+
+
