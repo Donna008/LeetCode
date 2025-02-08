2db13b30f59089880c6680540bda2718c4862a99
Donna <127694064+Donna008@users.noreply.github.com>
2025-02-08 16:10:00 -0700

    LC-top-k-frequent-elements [Runtime: N/A; Memory: N/A]

--- divide-and-conquer/top-k-frequent-elements.py
+++ divide-and-conquer/top-k-frequent-elements.py
@@ -1,6 +1,26 @@
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # count = Counter(nums)  # Creates a dictionary {num: frequency}
+        
+        # # Step 2: Use a heap to get the k most frequent elements
+        # return heapq.nlargest(k, count.keys(), key=count.get)
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = Counter(nums)  # Creates a dictionary {num: frequency}
-        
-        # Step 2: Use a heap to get the k most frequent elements
-        return heapq.nlargest(k, count.keys(), key=count.get)
+        n_list=[]
+        v=set()
+        for i in range(len(nums)):
+            if nums[i] in v:
+                continue
+            count=0
+            for j in range(len(nums)):
+                if nums[j] == nums[i]:
+                   count+=1
+            n_list.append((nums[j],count))
+            v.add(nums[i])
+        n_list.sort(key=lambda x: -x[1])
+        result = 0
+        for i in range(k):
+            result = n_list[i][0]
+        return result
+
+            
