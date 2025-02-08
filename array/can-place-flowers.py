dddc88be1bc13ddd7f8645bdc3086eac79aa727b
Donna <127694064+Donna008@users.noreply.github.com>
2025-02-07 19:50:59 -0700

    LC-can-place-flowers [Runtime: N/A; Memory: N/A]

---
+++ array/can-place-flowers.py
@@ -0,0 +1,41 @@
+# class Solution:
+#     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+#         length = len(flowerbed)
+        # i = 0
+
+        # while i < length:
+        #     if flowerbed[i] == 0:
+        #         left_empty = (i==0) or (flowerbed[i-1]==0)
+        #         right_empty = (i==length - 1) or (flowerbed[i+1]==0)
+                
+        #         if left_empty and right_empty:
+        #             flowerbed[i]==1
+        #             n -= 1
+        #             if n <= 0:
+        #                 return True
+        #             i += 2
+        #             continue
+        #     i += 1
+        # return n <= 0
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        length = len(flowerbed)
+        num=[]
+        for i in range(len(flowerbed)):
+            if flowerbed[i] or flowerbed[i]-1== 0:
+                num.append(flowerbed[i])
+                if n> len(num):
+                    return False
+                else:
+                    return True
+                # len(flowerbed[i]) > n:
+                #     return False
+                # else:
+                #     return True
+            
+                # if folwerbed[i]+1==0:
+
+
+
+
