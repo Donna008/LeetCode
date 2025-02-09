210659fed8566cda8c51d849b30aaa39cc7bbd4a
Donna <127694064+Donna008@users.noreply.github.com>
2025-02-08 22:26:34 -0700

    LC-last-stone-weight [Runtime: N/A; Memory: N/A]

--- array/last-stone-weight.py
+++ array/last-stone-weight.py
@@ -1,22 +1,28 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
+        # while len(stones) > 1:
+        #     y = max(stones)
+        #     stones.remove(y)
+        #     if stones:
+        #         x=max(stones)
+        #         stones.remove(x)
+        #         if x !=y:
+        #             stones.append(y-x)
+        # if stones:
+        #     return stones[0]
+        # else:
+        #     return 0          
+        # return stones[0] if stones else 0
+        stones=[-i for i in stones]
+        heapq.heapify(stones)
         while len(stones) > 1:
-            y = max(stones)
-            stones.remove(y)
-            if stones:
-                x=max(stones)
-                stones.remove(x)
-                if x !=y:
-                    stones.append(y-x)
+            y = -heapq.heappop(stones)
+            x = -heapq.heappop(stones)
+            if y!=x:
+                heapq.heappush(stones,-(y-x))
         if stones:
             return stones[0]
         else:
-            return 0          
-        # return stones[0] if stones else 0
-                #     if num!=num2:
-                #         sotones.append(num2 -num)
-                # if stones:
-                #     return stones[0]
-                # else:
-                #     return 0
+            return 0
+  
         
