c0e671366e16ac8dd92364cdbbacd0ab73a66504
Donna <127694064+Donna008@users.noreply.github.com>
2025-02-08 17:01:36 -0700

    LC-top-k-frequent-elements [Runtime: 0 ms; Memory: 21.3 MB]

--- hash-table/top-k-frequent-elements.py
+++ hash-table/top-k-frequent-elements.py
@@ -4,23 +4,44 @@
         
         # # Step 2: Use a heap to get the k most frequent elements
         # return heapq.nlargest(k, count.keys(), key=count.get)
+
+
+        # brute force
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         n_list=[]
+#         v=set()
+#         for i in range(len(nums)):
+#             if nums[i] in v:
+#                 continue
+#             count=0
+#             for j in range(len(nums)):
+#                 if nums[i] == nums[j]:
+#                    count+=1
+#             n_list.append((nums[i],count))
+#             v.add(nums[i])
+#         n_list.sort(key=lambda x: -x[1])
+#         result = []
+#         for i in range(k):
+#             result.append(n_list[i][0])
+#         return result
+
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        n_list=[]
-        v=set()
-        for i in range(len(nums)):
-            if nums[i] in v:
-                continue
-            count=0
-            for j in range(len(nums)):
-                if nums[i] == nums[j]:
-                   count+=1
-            n_list.append((nums[i],count))
-            v.add(nums[i])
-        n_list.sort(key=lambda x: -x[1])
-        result = []
-        for i in range(k):
-            result.append(n_list[i][0])
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:  
+        freq_map=Counter(nums)         
+        heap=[]
+        result=[]
+        for num, freq in freq_map.items():
+            heapq.heappush(heap,(freq,num))
+        # while head:
+            if len(heap)>k:
+                heapq.heappop(heap)
+        for freq, num in heap:
+            result.append(num)
         return result
 
-            
+
+
+
+
+
